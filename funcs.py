c170f7360562bdeafdecda87d7bc408cc0a1e8c9
Tavaninja <metalcorpe@gmail.com>
2018-07-08 17:17:03 +0300

    Line Space Generator (Numpy )

--- funcs.py
+++ funcs.py
@@ -7,11 +7,14 @@ def file_import(file):
 
 
 # class min_max:
-class AuthNet():
+class AuthNet:
 
-    def __init__(self, file):
+    def __init__(self, file, debug = False, N = 10):
         self.file = file
+        self.debug = debug
         self.id_table = file_import(file)
+        self.N = N
+
 
     def min(self):
         return min(self.id_table[:][3])
@@ -19,11 +22,18 @@ class AuthNet():
     def max(self):
         return max(self.id_table[:][3])
 
-
+    def lin_space(self):
+        from numpy import linspace
+        try:
+            lin = linspace(self.min(), self.max(), self.N)
+        except:
+            raise
+        return lin
 
 if __name__=="__main__":
     # imported_table = file_import("sx-stackoverflow_test.txt")
     # print(imported_table)
-    min = AuthNet("sx-stackoverflow_test.txt").min()
-    max = AuthNet("sx-stackoverflow_test.txt").max()
-    print(min, max)
+    net=AuthNet("sx-stackoverflow_test.txt")
+
+    # print(net.min(), net.max())
+    print(net.lin_space())
